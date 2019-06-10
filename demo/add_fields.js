fccfc651f37b3f5a70723b660e7a7b5c9462edb9
AmyChen <1621810784@qq.com>
2019-06-10 20:21:25 +0800

    标签自定义模板的设置样式更改

---
+++ demo/add_fields.js
@@ -0,0 +1,22 @@
+import i18next from '../locales'
+const addFields = [
+  { key: i18next.t('商品名'), value: i18next.t('{{SKU}}') },
+  { key: i18next.t('商品ID'), value: i18next.t('{{SKU_ID}}') },
+  { key: i18next.t('下单数'), value: i18next.t('{{下单}}') },
+  { key: i18next.t('实称数'), value: i18next.t('{{实称}}') },
+  { key: i18next.t('分拣号'), value: i18next.t('{{分拣号}}') },
+  { key: i18next.t('司机'), value: i18next.t('{{司机}}') },
+  { key: i18next.t('商户名'), value: i18next.t('{{商户名}}') },
+  { key: i18next.t('商户ID'), value: i18next.t('{{商户ID}}') },
+  { key: i18next.t('收货人'), value: i18next.t('{{收货人}}') },
+  { key: i18next.t('路线'), value: i18next.t('{{路线}}') },
+  { key: i18next.t('站点名'), value: i18next.t('{{站点名}}') },
+  { key: i18next.t('备注'), value: i18next.t('{{备注}}') },
+  { key: i18next.t('客服电话'), value: i18next.t('{{客服电话}}') },
+  { key: i18next.t('溯源码'), value: i18next.t('{{溯源码}}') },
+  { key: i18next.t('商品验货码'), value: i18next.t('{{商品码}}') },
+  { key: i18next.t('自定义编码'), value: i18next.t('{{自定义编码}}') },
+  { key: i18next.t('打印时间'), value: i18next.t('{{当前时间_年月日}}') },
+  { key: i18next.t('页码'), value: i18next.t('{{页码}}') }
+]
+export default addFields
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Flex from './flex'
+import { Select, Option } from './select'
+import Tip from './tip'
+import { DropDown, DropDownItems, DropDownItem } from './dropdown'
+import Dialog from './dialog'
+
+export {
+  Flex,
+  Select, Option,
+  Tip,
+  DropDown, DropDownItems, DropDownItem,
+  Dialog
+}
