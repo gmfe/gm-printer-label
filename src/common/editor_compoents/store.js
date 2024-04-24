fc486f44394a89b21d8a7e36dc6b5132ba56b958
xujiahao <xujiahao@guanmai.cn>
2024-04-24 14:31:27 +0800

    feat: add_customer_barcode

--- src/common/editor_compoents/store.js
+++ src/common/editor_compoents/store.js
@@ -18,7 +18,7 @@ class EditStore {
   originConfig = null
 
   @action.bound
-  init (config, initDefaultTemp) {
+  init(config, initDefaultTemp) {
     this.config = this.getHandledConfig(config)
     this.originConfig = this.getHandledConfig(config)
     this.selected = null
@@ -26,46 +26,46 @@ class EditStore {
   }
 
   @action.bound
-  setConfig (config, value) {
+  setConfig(config, value) {
     this.selected = null
     this.config = { ...this.config, ...config }
     this.tempKey = value
   }
 
   @action.bound
-  setConfigName (name) {
+  setConfigName(name) {
     this.config.name = name
   }
 
   @action.bound
-  setSizePageType (type) {
+  setSizePageType(type) {
     this.config.page.type = type
   }
 
   @action.bound
-  setCustomizePageSize (name, value) {
+  setCustomizePageSize(name, value) {
     this.config.page[name] = value
   }
 
   @action.bound
-  setPageName (name) {
+  setPageName(name) {
     this.config.name = name
   }
 
   @action.bound
-  setSelected (selected = null) {
+  setSelected(selected = null) {
     this.selected = selected
   }
 
   @action.bound
-  setConfigBlockBy (who, value) {
+  setConfigBlockBy(who, value) {
     if (this.selected !== null) {
       this.config.blocks[this.selected][who] = value
     }
   }
 
   @action.bound
-  addConfigBlock (type, url) {
+  addConfigBlock(type, url) {
     switch (type) {
       case '':
       case 'text':
@@ -75,8 +75,8 @@ class EditStore {
             position: 'absolute',
             fontSize: '14px',
             left: '0px',
-            top: '0px'
-          }
+            top: '0px',
+          },
         })
         break
       case 'line':
@@ -89,8 +89,8 @@ class EditStore {
             borderTopColor: 'black',
             borderTopWidth: '1px',
             borderTopStyle: 'solid',
-            width: '100%'
-          }
+            width: '100%',
+          },
         })
         break
       case 'qrcode':
@@ -102,8 +102,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '50px',
-            height: '50px'
-          }
+            height: '50px',
+          },
         })
         break
       case 'merchandise_trace_qrcode':
@@ -115,8 +115,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '50px',
-            height: '50px'
-          }
+            height: '50px',
+          },
         })
         break
       case 'verification_qrcode':
@@ -128,8 +128,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '50px',
-            height: '50px'
-          }
+            height: '50px',
+          },
         })
         break
       case 'order_qrcode':
@@ -141,8 +141,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '50px',
-            height: '50px'
-          }
+            height: '50px',
+          },
         })
         break
       case 'barcode':
@@ -154,8 +154,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
         })
         break
       case 'in_stock_barcode':
@@ -167,8 +167,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
         })
         break
       case 'package_id_qrcode':
@@ -180,8 +180,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '50px',
-            height: '50px'
-          }
+            height: '50px',
+          },
         })
         break
       case 'diycode':
@@ -193,8 +193,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
         })
         break
       case 'diyqrcode':
@@ -207,8 +207,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '80px',
-            height: '80px'
-          }
+            height: '80px',
+          },
         })
         break
       case 'image':
@@ -220,8 +220,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '100px',
-            width: '100px'
-          }
+            width: '100px',
+          },
         })
         break
       case 'production_barcode':
@@ -233,8 +233,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
         })
         break
       case 'new_production_barcode':
@@ -246,8 +246,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
         })
         break
       case 'rack_barcode':
@@ -259,8 +259,21 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '30px',
-            width: '165px'
-          }
+            width: '165px',
+          },
+        })
+        break
+      case 'customer_barcode':
+        this.config.blocks.push({
+          type,
+          customer_barcode: i18next.t('{{客户码}}'),
+          style: {
+            position: 'absolute',
+            left: '0px',
+            top: '0px',
+            height: '30px',
+            width: '165px',
+          },
         })
         break
       case 'delivery_qrcode':
@@ -272,8 +285,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             height: '80px',
-            width: '80px'
-          }
+            width: '80px',
+          },
         })
         break
       case 'package_order_qrcode':
@@ -285,8 +298,8 @@ class EditStore {
             left: '0px',
             top: '0px',
             width: '60px',
-            height: '60px'
-          }
+            height: '60px',
+          },
         })
         break
       default:
@@ -297,7 +310,7 @@ class EditStore {
 
   // 删除所选字段
   @action.bound
-  removeConfig () {
+  removeConfig() {
     if (this.selected !== null) {
       this.config.blocks.splice(this.selected, 1)
       this.selected = null
@@ -306,30 +319,30 @@ class EditStore {
 
   // 添加字段到打印单中 两种 ：1-添加到打印的模板中 2-添加到自定义二维码中
   @action.bound
-  addFieldToBlocks ({ value, key, fieldType }) {
+  addFieldToBlocks({ value, key, fieldType }) {
     // 不一定每次打印都传页码过去，为了不出现“页码：”情况，页码前不加前缀
     if (!this.config.blocks[this.selected]?.diyqrcode) {
       key === i18next.t('页码')
         ? this.config.blocks.push({
-          text: `${value}`,
-          fieldType,
-          fieldKey: key,
-          style: {
-            position: 'absolute',
-            left: '0px',
-            top: '0px'
-          }
-        })
+            text: `${value}`,
+            fieldType,
+            fieldKey: key,
+            style: {
+              position: 'absolute',
+              left: '0px',
+              top: '0px',
+            },
+          })
         : this.config.blocks.push({
-          text: `${key}:${value}`,
-          fieldType,
-          fieldKey: key,
-          style: {
-            position: 'absolute',
-            left: '0px',
-            top: '0px'
-          }
-        })
+            text: `${key}:${value}`,
+            fieldType,
+            fieldKey: key,
+            style: {
+              position: 'absolute',
+              left: '0px',
+              top: '0px',
+            },
+          })
     }
     if (this.config.blocks[this.selected]?.diyqrcode) {
       this.config.blocks[this.selected].text += value + ','
@@ -337,7 +350,7 @@ class EditStore {
   }
 
   @computed
-  get computedIsTime () {
+  get computedIsTime() {
     if (this.selected !== null) {
       const block = this.config.blocks[this.selected]
       if (
@@ -351,14 +364,14 @@ class EditStore {
   }
 
   @action.bound
-  getHandledConfig (config) {
+  getHandledConfig(config) {
     const result = _.cloneDeep(config)
     // 添加自定义尺寸字段
     result.page = {
       ...result.page,
       customizeWidth: result.page.customizeWidth || 100, // 若没有则默认为100
       customizeHeight: result.page.customizeHeight || 100,
-      doublePage: result.page.doublePage || false
+      doublePage: result.page.doublePage || false,
     }
 
     return result
