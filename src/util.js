28895387228b5f700b2eb6fce5e27cdf96c384c7
Zhou-Bill <735051883@qq.com>
2025-07-24 17:39:34 +0800

    feat: 增加template 函数

--- src/util.js
+++ src/util.js
@@ -44,6 +44,50 @@ function dispatchMsg(event, data) {
 function substring(target, start = 0, end) {
   return target.substring(start, end)
 }
+
+
+const price = (n, f = 2) => {
+  /** 有些价格会有...,传给Big会报错，那么替换掉 */
+  if (typeof n === 'string') {
+    n = n.replace(/\.+$/, '')
+  }
+  // 自定义函数支持多栏
+  if (n === undefined || n === '') return ''
+  return Big(n || 0).toFixed(f)
+}
+const diyRandom = (a, b, c = 2) => {
+  return (a + Math.random() * (b - a)).toFixed(c)
+}
+const parseFloatFun = a => {
+  // 自定义函数支持多栏
+  if (a === '' || a === undefined) return ''
+  return parseFloat(+a)
+}
+
+/**
+ *
+ * @param num 处理小数点末尾零
+ * @returns
+ */
+export function formatDecimal(num) {
+  // 将输入转为字符串
+  const str = String(num)
+
+  // 检查是否是整数（不含小数点）
+  if (!str.includes('.')) return str
+
+  // 处理小数部分
+  let [integerPart, decimalPart] = str.split('.')
+
+  // 移除小数部分末尾的所有零
+  decimalPart = decimalPart.replace(/0+$/, '')
+
+  // 返回结果
+  return decimalPart.length
+    ? `${integerPart}.${decimalPart}` // 保留非零小数
+    : integerPart // 没有小数部分时只返回整数
+}
+
 function template(text, data) {
   try {
     return _.template(text, {
@@ -51,6 +95,10 @@ function template(text, data) {
     })({
       ...data,
       substring: substring, // 添加一个截取字符串函数
+      price: price,
+      diyRandom: diyRandom, // 提供一个计算随机数的函数
+      parseFloatFun: parseFloatFun,
+      formatDecimal: formatDecimal
     })
   } catch (err) {
     console.warn(err)
