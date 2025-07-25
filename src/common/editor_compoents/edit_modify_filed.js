f162c531eda25322c2ad1a43913ffc4b1398f13d
Yiiu <yu.gintoki@gmail.com>
2025-07-25 15:10:23 +0800

    fix: 【1042422】【ERP】【早晚生鲜】分拣标签与包装内标标签需支持打出商品到期时间

--- src/common/editor_compoents/edit_modify_filed.js
+++ src/common/editor_compoents/edit_modify_filed.js
@@ -179,7 +179,7 @@ class EditModifyFiled extends React.Component {
             {_.map(DiyTimeType, (v, k) => (
               <TipInfo text={`${k + 1}、${v.text}`}/>
             ))}
-            <TipInfo text={`${DiyTimeType.length + + 1}、指定日期后7天，输入“单据日期：{{addDay(单据日期, 7)}}”`}/>
+            <TipInfo text={`${DiyTimeType.length + + 1}、指定日期后7天，输入“单据日期：{{addDay(单据日期, 7)}}”；`}/>
           </div>
         )}
       </React.Fragment>
